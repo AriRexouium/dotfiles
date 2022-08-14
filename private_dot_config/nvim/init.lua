2821c771f7f098873fa400ad9155847a375fa47c
Jarrett Aiken <jarrett.aiken@achl.fr>
2022-08-13 20:13:30 -0400

    Update Neovim & Tmux plugin manager bootstrapping.

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -26,13 +26,6 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd [[packadd packer.nvim]]
 end
 
-vim.cmd[[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost init.lua source <afile> | PackerSync
-  augroup end
-]]
-
 -- Configuration -------------------------------------------------------------------------------------------------------
 -- Enable line numbers. --------------------------------------------------------
 o.number = true
