6287276c866a3d4ce6a4b85e1bd0342b53fcf44b
Jarrett Aiken <jarrett.aiken@achl.fr>
2023-12-03 14:56:16 -0500

    Remove btop config, update fish.

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,37 +1,19 @@
-# Add XDG support prior to anything else.
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_CACHE_HOME $HOME/.cache
-set -x XDG_DATA_HOME $HOME/.local/share
-set -x XDG_STATE_HOME $HOME/.local/state
-
-# Set prompt settings.
-starship init fish | source
 set fish_greeting
-
-# Set various SSH & GPG parameters.
-if string match -e 'kitty' $TERM; alias ssh 'kitty +kitten ssh'; end
-set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
-set -x GPG_TTY $(tty)
-
-# User bin support.
+#starship init fish | source
 set -gx PATH "$HOME/.local/bin" $PATH
+# set -x GPG_TTY $(tty)
 
-# pnpm
-set -gx PNPM_HOME "$XDG_DATA_HOME/pnpm"
-set -gx PATH "$PNPM_HOME" $PATH
-alias pnpx 'pnpm exec'
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
-
-# Custom aliases.
-# alias cat 'bat'; alias find 'fd'; alias grep 'rg'; alias ps 'procs'
+# List Files
 alias ls 'exa --classify --icons --git'
 alias li 'ls --long --header --group --modified --created --octal-permissions'
 alias tree 'ls --tree'
 
+# Docker
+alias dl "docker logs -f"
+alias dls "docker ps -a --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}'"
 alias dcu 'docker compose up -d'
 alias dcd 'docker compose down --remove-orphans'
 alias dcl 'docker compose logs -f'
-alias dcls "docker container ls --format 'table {{.ID}}\t{{.Names}}\t{{.Image}}\t{{.RunningFor}}\t{{.Status}}'"
 
-alias tb 'nc termbin.com 9999'
-alias pacman-packages 'pacman -Qq | fzf --preview \'pacman -Qil {}\' --layout=reverse --bind \'enter:execute(pacman -Qil {} | less)\''
+# termbin
+alias termbin 'nc termbin.com 9999'
